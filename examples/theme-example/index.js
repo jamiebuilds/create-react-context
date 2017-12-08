a869d927823d24d16a08a7c7799b8b2aa415601e
pkodmad <pkodmad@atlassian.com>
2017-12-08 16:11:22 +1100

    Add another component that accepts context

--- examples/theme-example/index.js
+++ examples/theme-example/index.js
@@ -1,11 +1,12 @@
 //@flow
 import React from "react";
 import { render } from "react-dom";
-import { ThemeToggler, Title } from "./theme-context";
+import { ThemeToggler, Title, Emoji } from "./theme-context";
 
 const App = () => (
   <ThemeToggler>
     <Title>Really cool context</Title>
+    <Emoji />
   </ThemeToggler>
 );
 
