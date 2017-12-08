f0f1b2ac45c21a63d1e93d2967469e8e500c00b0
pkodmad <pkodmad@atlassian.com>
2017-12-08 15:27:59 +1100

    Add a working example to demo create-react-context

---
+++ examples/theme-example/index.js
@@ -0,0 +1,10 @@
+//@flow
+import React from 'react';
+import {render} from 'react-dom';
+import {ThemeToggler, Title} from './theme-context';
+
+const App = () => (<ThemeToggler>
+        <Title>Really cool context</Title>
+    </ThemeToggler>)
+
+render(<App />, document.getElementById('container'))
